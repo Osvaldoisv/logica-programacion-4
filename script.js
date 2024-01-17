36a76eb2799ecd199e51390f6c5e7c18c97ca74b
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-01-17 12:12:48 -0300

    lista en html incluida

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function borrarLista(lista){
+    while (lista.firstChild) {              // Se selecciona el primer "hijo"
+        lista.removeChild(lista.firstChild);    // Se elimina
+    }
+    return lista;
+}
+
+
+
 // Al dar click en el botón, se ejecuta la siguiente función
 function Fibonacci(){
 
     // Declaramos las variables provenientes del html (el input y el resultado mostrado)
     let entrada = document.getElementById("Entrada").value;
     let resultado = document.getElementById("Resultado");
+    let lista = document.getElementById("lista-nueva");     // Para borrar elementos de lista a futuro
 
     // Generamos los valores iniciales de la secuencia de Fibonacci
     let listaInicial = [0,1,1];
@@ -15,6 +25,7 @@ function Fibonacci(){
         if (entrada <= 0){
             resultado.innerHTML = "No hay posiciones negativas ni posición cero, intenta con otro numero";
             resultado.style.color = "red"; 
+            borrarLista(lista);
             
         } else {
 
@@ -23,31 +34,29 @@ function Fibonacci(){
                 nuevoNumero = listaInicial[n] + listaInicial[n-1];
                 listaInicial.push(nuevoNumero);
 
-                // let nuevaLista = document.createElement("li");
-                // let elementoNuevo = document.createTextNode(nuevoNumero);
-                // nuevaLista.appendChild(elementoNuevo);
-                // document.getElementById("lista-nueva").appendChild(nuevaLista);
             }
 
             // Imprimimos resultado
-
             let rango = listaInicial.slice(0,entrada);
-            console.log(rango)
             resultado.style.color = "black";
-            resultado.innerHTML = "Resultado: " + rango;
+            resultado.innerHTML = "Resultado final: " + rango[listaInicial.length-1] + " en posición: " + rango.length;
 
+            borrarLista(lista); // Borramos elementos de lista
 
-
-            // let nuevaLista = document.createElement("li");
-            // let adios = document.createTextNode("adios")
-            // nuevaLista.appendChild(adios);
-            // document.getElementById("lista-nueva").appendChild(nuevaLista);
+            // Creamos lista para HTML
+            for (let i = 0; i < entrada; i++){
+                let nuevaLista = document.createElement("li");      // Creamos elemento hijo para lista en html
+                let elementoNuevo = document.createTextNode(rango[i]);      // Al elemento hijo le añadimos la variable correspondiente
+                nuevaLista.appendChild(elementoNuevo);                  // Añadimos elemento hijo a la lista
+                document.getElementById("lista-nueva").appendChild(nuevaLista);     // Publicamos lista en el DOM
+            }
         }
     } else {
 
         // Si no es número, imprimimos mensaje
         resultado.innerHTML = "Ingrese un número válido";
         resultado.style.color = "red";
+        borrarLista(lista);
     }
     
 }
